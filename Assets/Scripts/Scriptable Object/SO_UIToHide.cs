00ac79ea8b41722bb875f7baa759fbde4e910ef5
Johannes Kromer <info@pencil-me.de>
2020-08-02 23:11:35 +0200

    some cleanup

--- Assets/Scripts/Scriptable Object/SO_UIToHide.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AssetData", menuName = "UI/UIToHide Data")]
-public class SO_UIToHide : ScriptableObject
-{
-    [SerializeField] GameObject[] RegionsToHide;
-}
